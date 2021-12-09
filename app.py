457fa8b05d2cfb128e6eea020240aee289bf44ef
snhryt@mbp <snhryt@gmail.com>
2021-12-09 15:02:26 +0900

    :tada: Implement word replace function

--- app.py
+++ app.py
@@ -1,45 +1,49 @@
 #!/usr/bin/env python3
-import datetime as dt
 from typing import List
 
 import numpy as np
 import pandas as pd
 import streamlit as st
-from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, HoverTool
+
+# from bokeh.plotting import figure
+# from bokeh.models import ColumnDataSource, HoverTool
 
 from catchphrase import Catchphrase
+from word_replacer import WordReplacer
 
 
-@st.cache(allow_output_mutation=True)
-def init() -> Catchphrase:
+@st.experimental_singleton
+def store_embedded_catchphrases() -> Catchphrase:
     cp = Catchphrase()
     if len(cp.df) == 0:
         cp.crawl_embed(is_init=True)
     return cp
 
 
-@st.cache
-def convert_df(df: pd.DataFrame) -> pd.DataFrame:
-    return df.to_csv().encode("UTF-8")
+@st.experimental_singleton
+def init_word_replacer() -> WordReplacer:
+    return WordReplacer()
 
 
-@st.cache
-def calc_mds(
-    cp: Catchphrase,
-    phrase: str,
-    max_phrase_length: int,
-    target_categories: List[str],
-    target_atmospheres: List[str],
-    target_users: List[str],
-):
-    return cp.calc_mds(
-        phrase,
-        max_phrase_length=max_phrase_length,
-        target_categories=target_categories,
-        target_atmospheres=target_atmospheres,
-        target_users=target_users,
-    )
+# @st.experimenta_singleton
+# def convert_df(df: pd.DataFrame) -> pd.DataFrame:
+#     return df.to_csv().encode("UTF-8")
+# @st.experimenta_singleton
+# def calc_mds(
+#     cp: Catchphrase,
+#     phrase: str,
+#     max_phrase_length: int,
+#     target_categories: List[str],
+#     target_atmospheres: List[str],
+#     target_users: List[str],
+# ):
+#     return cp.calc_mds(
+#         phrase,
+#         max_phrase_length=max_phrase_length,
+#         target_categories=target_categories,
+#         target_atmospheres=target_atmospheres,
+#         target_users=target_users,
+#     )
 
 
 def create_candidates(cp: Catchphrase, col: str) -> List[str]:
@@ -50,7 +54,7 @@ def main():
     st.set_page_config(page_title="キャッチコピー生成のプロトタイプ")
 
     # スクレイピングしたキャッチコピーおよび学習済モデルの読み込み
-    cp = init()
+    cp = store_embedded_catchphrases()
 
     st.title("キャッチコピー生成のプロトタイプ")
     st.markdown("キャッチコピーのスクレイピング元: [キャッチコピー集めました。](https://catchcopy.make1.jp/)")
@@ -107,69 +111,135 @@ def main():
     desc_calc_df = calc_df.sort_values("類似度", ascending=False)[:n_displays]
     asc_calc_df = calc_df.sort_values("類似度", ascending=True)[:n_displays]
 
-    # ダウンロードリンク用の情報
-    # desc_calc_csv = convert_df(desc_calc_df)
-    # desc_calc_filename = f"類似度top{n_displays}_{dt.date.today()}.csv"
-    # asc_calc_csv = convert_df(asc_calc_df)
-    # asc_calc_filename = f"類似度bottom{n_displays}_{dt.date.today()}.csv"
-
     similarity_container = st.container()
     col1, col2 = similarity_container.columns(2)
     col1.subheader("類似度が高い順")
     col1.table(desc_calc_df[["キャッチコピー", "類似度"]])
-    # col1.download_button(
-    #     label="download",
-    #     data=desc_calc_csv,
-    #     file_name=desc_calc_filename,
-    #     mime="text/csv",
-    # )
     col2.subheader("類似度が低い順")
     col2.table(asc_calc_df[["キャッチコピー", "類似度"]])
-    # col2.download_button(
-    #     label="download",
-    #     data=asc_calc_csv,
-    #     file_name=asc_calc_filename,
-    #     mime="text/csv",
-    # )
 
-    # st.markdown("---")
+    st.markdown("---")
     # ==============================================================================
-
-    # 選択されたキャッチコピーの平均算出
-    # ==============================================================================
-    candidates = pd.concat([desc_calc_df["キャッチコピー"], asc_calc_df["キャッチコピー"]]).unique()
-    selected_phrases = st.multiselect(
-        "上記の中で「このニュアンス/雰囲気使いたいな」と思ったキャッチコピーがあれば選択してください（2〜3個程度）", candidates
+    func_mode = st.selectbox(
+        "次に実行する処理を選択してください", ["-", "ニュアンス/雰囲気の類似したキャッチコピー検索", "キャッチコピー内の単語の類語への置き換え"]
     )
-    if len(selected_phrases) == 0:
-        st.stop()
+    st.markdown("---")
 
-    embedded_selected_phrases = calc_df[calc_df["キャッチコピー"].isin(selected_phrases)][
-        "埋め込み済キャッチコピー"
-    ]
-    vectors = np.array([vec for vec in embedded_selected_phrases])
-    similar_phrases = cp.calc_similarity_to_phrases(
-        phrase="",
-        embedded_phrase=vectors.mean(axis=0).reshape(1, len(vectors[0])),
-        max_phrase_length=max_phrase_length,
-        target_categories=target_categories,
-        target_atmospheres=target_atmospheres,
-        target_users=target_users,
-    ).sort_values("類似度", ascending=False)["キャッチコピー"]
+    func_container = st.container()
+    with func_container:
+        candidates = pd.concat(
+            [desc_calc_df["キャッチコピー"], asc_calc_df["キャッチコピー"]]
+        ).unique()
 
-    st.subheader("ニュアンス/雰囲気の類似したキャッチコピー")
-    n_mean_displays = st.slider("キャッチコピー表示数", min_value=1, max_value=20, value=5)
-    counter = 0
-    for phrase in similar_phrases:
-        if phrase in selected_phrases:
-            continue
-        st.markdown(f"`{phrase}`")
-        counter += 1
-        if counter == n_mean_displays:
-            break
+        if func_mode == "ニュアンス/雰囲気の類似したキャッチコピー検索":
+            st.subheader(func_mode)
 
-    st.info("※ いまは平均から新規キャッチコピーの生成ではなく、類似した既存キャッチコピーの検索しか行えていない")
-    # ==============================================================================
+            selected_phrases = st.multiselect(
+                "上記の中で「このニュアンス/雰囲気使いたいな」と思ったキャッチコピーがあれば選択してください（2〜3個程度）", candidates
+            )
+            if len(selected_phrases) == 0:
+                st.stop()
+
+            embedded_selected_phrases = calc_df[
+                calc_df["キャッチコピー"].isin(selected_phrases)
+            ]["埋め込み済キャッチコピー"]
+            vectors = np.array([vec for vec in embedded_selected_phrases])
+            similar_phrases = cp.calc_similarity_to_phrases(
+                phrase="",
+                embedded_phrase=vectors.mean(axis=0).reshape(1, len(vectors[0])),
+                max_phrase_length=max_phrase_length,
+                target_categories=target_categories,
+                target_atmospheres=target_atmospheres,
+                target_users=target_users,
+            ).sort_values("類似度", ascending=False)["キャッチコピー"]
+
+            n_mean_displays = st.slider(
+                "キャッチコピー表示数", min_value=1, max_value=20, value=5
+            )
+            counter = 0
+            for phrase in similar_phrases:
+                if phrase in selected_phrases:
+                    continue
+                st.markdown(f"`{phrase}`")
+                counter += 1
+                if counter == n_mean_displays:
+                    break
+
+            st.info("※ いまは平均から新規キャッチコピーの生成ではなく、類似した既存キャッチコピーの検索しか行えていない")
+
+        elif func_mode == "キャッチコピー内の単語の類語への置き換え":
+            st.subheader(func_mode)
+
+            selected_phrase = st.selectbox(
+                "上記の中で「ベースにしたいな」と思ったキャッチコピーがあれば選択してください", ["-"] + candidates.tolist()
+            )
+            if selected_phrase == "-":
+                st.stop()
+
+            word_replacer = init_word_replacer()
+            surfaces = word_replacer.wakati(selected_phrase)
+            part_of_speechs = [
+                part_of_speech[0]
+                for part_of_speech in word_replacer.wakati(selected_phrase, mode=4)
+            ]
+
+            part_of_speech_choices = set(part_of_speechs) & set(
+                ["動詞", "名詞", "代名詞", "形容詞", "形容動詞", "副詞"]
+            )
+            part_of_speech_targets = st.multiselect(
+                "置換対象にする品詞を選択してください（複数選択可）", part_of_speech_choices
+            )
+            if len(part_of_speech_targets) == 0:
+                st.stop()
+
+            replacing_target_surfaces = set(
+                [
+                    surface
+                    for surface, part_of_speech in zip(surfaces, part_of_speechs)
+                    if part_of_speech in part_of_speech_targets
+                ]
+            )
+            if len(replacing_target_surfaces) == 0:
+                # st.error("キャッチコピーに含まれる品詞を1つ以上選択してください")
+                st.stop()
+
+            target_surface = st.radio(
+                "置換したい単語を1つ選択してください", ["-"] + list(replacing_target_surfaces)
+            )
+            st.markdown("---")
+
+            top_N = st.slider("最大表示キャッチコピー数", 3, 30, value=10)
+            normalization = st.checkbox(
+                "置換前の単語を正規化する（例: 附属 → 付属、SUMMER → サマー、シュミレーション → シミュレーション）"
+            )
+            if target_surface == "-":
+                st.stop()
+
+            with st.spinner("類似語に置換した文章生成中..."):
+                replaced_phrases = word_replacer.generate_replaced_sentences(
+                    selected_phrase,
+                    target_surface,
+                    top_N=top_N,
+                    normalization=normalization,
+                )
+
+            left, right = st.columns(2)
+            left.markdown("**置換後**")
+            for phrase in replaced_phrases:
+                left.markdown(phrase)
+            right.markdown("**オリジナル**")
+            if normalization:
+                replaced_phrase = selected_phrase.replace(
+                    target_surface, f"`{word_replacer.normalize(target_surface)}`", 1
+                )
+            else:
+                replaced_phrase = selected_phrase.replace(
+                    target_surface, f"`{target_surface}`", 1
+                )
+            right.markdown(replaced_phrase)
+
+        else:
+            st.stop()
 
     # TODO: 実装する
     # MDSによるキャッチコピーの二次元分布図
